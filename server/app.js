5251daace57d8e2aa34e6890db7cc5c291da7371
selenabell <itgamtics4@gmail.com>
2021-05-29 00:12:58 -0500

    Instalando Prettier

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
@@ -7,45 +8,46 @@ import logger from 'morgan';
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
 
-//Webpack modules
-import webpack from 'webpack'; 
+// Webpack modules
+import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.dev.config';
-//import webpackDevConfig from '../webpack.dev.config';
 
-//consultar el modo en que se esta ejecutando la aplicacion
+// consultar el modo en que se esta ejecutando la aplicacion
 
 const env = process.env.NODE_ENV || 'development';
 
-//Se crea aplicación express
-var app = express();
+// Se crea aplicación express
+const app = express();
 
-//Verifica el modo de ejecución
+// Verifica el modo de ejecución
 
-if(env === 'development'){
-  
+if (env === 'development') {
   console.log('>Excecuting in Development Mode: Webpack hot Reloading');
-  //1.Agregando la ruta HMR
-  //reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend
-  //timeout=1000: tiempo de espera entre recarga y recarga de la pagin
-  webpackConfig.entry =['webpack-hot-middleware/client?reload=true&timeout=1000',webpackConfig.entry];
-//Paso 2. Agregar el plugin
-webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-//paso 3. Crear el compliador de webpack 
-const compiler=webpack(webpackConfig);
+  // 1.Agregando la ruta HMR
+  // reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del front-end
+  // timeout=1000: tiempo de espera entre recarga y recarga de la pagina
+  webpackConfig.entry = [
+    'webpack-hot-middleware/client?reload=true&timeout=1000',
+    webpackConfig.entry,
+  ];
+  // Paso 2. Agregar el plugin
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // paso 3. Crear el compliador de webpack
+  const compiler = webpack(webpackConfig);
 
-//paso 4. Agregando el Middleware a la cadena de Middelewares de nuesta aplicación
-app.use(webpackDevMiddleware(compiler,{
-  publicPath:webpackConfig.output.publicPath
-  //Paso 5. Agregando el webpack hot Middleware
- 
-}));
-app.use(webpackHotMiddleware(compiler));
-}else {
+  // paso 4. Agregando el Middleware a la cadena de Middelewares de nuesta aplicación
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      // Paso 5. Agregando el webpack hot Middleware
+    })
+  );
+  app.use(webpackHotMiddleware(compiler));
+} else {
   console.log('>Excecuting in Production Mode');
 }
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -54,18 +56,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
